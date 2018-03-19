4cd027a8c42c8ea88b9afde8e8f5a30c17d144e7
MAX CAI <cenylcz@MAXs-MBP.attlocal.net>
2018-03-18 18:23:00 -0700

    SE-0003: Add new schema-Client

---
+++ src/main/java/com/cenylcz/domain/client/User.java
@@ -0,0 +1,67 @@
+package com.cenylcz.domain.client;
+
+import com.cenylcz.Model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user", schema = "client")
+public class User implements Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_key", unique = true, nullable = false)
+    private Integer userKey;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "boa", nullable = false)
+    private Date boa;
+
+    public Integer getUserKey() {
+        return userKey;
+    }
+
+    public void setUserKey(Integer userKey) {
+        this.userKey = userKey;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBoa() {
+        return boa;
+    }
+
+    public void setBoa(Date boa) {
+        this.boa = boa;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userKey=" + userKey +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", boa=" + boa +
+                '}';
+    }
+}
--- src/main/resources/new.sql
+++ src/main/resources/new.sql
@@ -1,12 +1,22 @@
-create table transaction (
-	transaction_key INT NOT NULL AUTO_INCREMENT,
-	amount double(8, 2) NOT NULL,
-	transaction_date DATE NOT NULL,
-	purchased_method ENUM('CREDIT_CARD', 'CASH', 'CHECK', 'TRANSFER') NOT NULL,
-	category ENUM('GAS', 'GROCERY', 'DINING', 'TRAVEL', 'UTILITY', 'MEDICARE','CLOTHES', 'APPLIANCES', 'COSMETIC') NOT NULL,
-	merchant VARCHAR(50) NOT NULL,
-	saving double(8, 2) DEFAULT 0,
-	PRIMARY KEY(transaction_key)
+create table `business`.`transaction` (
+	`transaction_key` INT NOT NULL AUTO_INCREMENT,
+	`amount` DOUBLE(8, 2) NOT NULL,
+	`transaction_date` DATE NOT NULL,
+	`purchased_method` ENUM('CREDIT_CARD', 'CASH', 'CHECK', 'TRANSFER') NOT NULL,
+	`category` ENUM('GAS', 'GROCERY', 'DINING', 'TRAVEL', 'UTILITY', 'MEDICARE','CLOTHES', 'APPLIANCES', 'COSMETIC') NOT NULL,
+	`merchant` VARCHAR(50) NOT NULL,
+	`saving` DOUBLE(8, 2) DEFAULT 0,
+	PRIMARY KEY(`transaction_key`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table `client`.`user` (
+	`user_key` INT NOT NULL AUTO_INCREMENT,
+	`first_name` VARCHAR(25),
+	`last_name` VARCHAR(25),
+	`boa` DATE NOT NULL,
+	PRIMARY KEY(`user_key`)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
