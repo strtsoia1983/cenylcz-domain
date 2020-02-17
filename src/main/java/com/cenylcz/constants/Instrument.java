d7ddf442a9abbf246a8bb7466a86637d81ee7b06
MAX CAI <cenylcz@MAXs-MBP.attlocal.net>
2020-02-16 20:15:30 -0800

    Remove helm from domain

--- src/main/java/com/cenylcz/constants/Instrument.java
+++ src/main/java/com/cenylcz/constants/Instrument.java
@@ -5,7 +5,8 @@ public enum Instrument {
     EUR_USD("UER_USD"),
     NZD_CHF("NZD_CHF"),
     AUD_CHF("AUD_CHF"),
-    CAD_CHF("CAD_CHF");
+    CAD_CHF("CAD_CHF"),
+    USD_CHF("USD_CHF");
 
     private final String instrument;
 
