047781db74c6ee6dca249858828d5c96207c92c5
cenylcz <cenylcz@gmail.com>
2020-05-19 18:19:10 -0700

    Remove annotation

--- src/main/java/com/cenylcz/Model.java
+++ src/main/java/com/cenylcz/Model.java
@@ -1,14 +1,4 @@
 package com.cenylcz;
 
-import com.cenylcz.domain.business.Forex;
-import com.cenylcz.domain.business.Stock;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = Stock.class, name = "stock"),
-        @JsonSubTypes.Type(value = Forex.class, name = "forex")
-})
 public abstract class Model {
 }
