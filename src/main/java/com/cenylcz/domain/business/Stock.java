a3bd4f691e675340853f678742369090f13dccbb
cenylcz <cenylcz@gmail.com>
2020-05-20 21:11:42 -0700

    Add new domain

--- src/main/java/com/cenylcz/domain/business/Stock.java
+++ src/main/java/com/cenylcz/domain/business/Stock.java
@@ -1,87 +1,25 @@
 package com.cenylcz.domain.business;
 
-import com.cenylcz.constants.StockStatus;
-import com.cenylcz.constants.TradePlatform;
-import com.cenylcz.convert.StockStatusConvert;
-import com.cenylcz.convert.TradePlatformConvert;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.cenylcz.Model;
 import javax.persistence.*;
-import java.io.Serializable;
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "stock", schema = "business")
-public class Stock extends Ticket implements Serializable {
-
-    public Stock() {
-
-    }
-
-    @JsonCreator
-    public Stock(@JsonProperty(required = true, value = "stockKey") Integer stockKey,
-                 @JsonProperty(required = true, value = "symbol") String symbol,
-                 @JsonProperty(required = true, value = "fullName") String fullName,
-                 @JsonProperty(required = true, value = "quantity") Integer quantity,
-                 @JsonProperty(required = true, value = "openUnitCost") Double openUnitCost,
-                 @JsonProperty(required = true, value = "closeUnitCost") Double closeUnitCost,
-                 @JsonProperty(required = true, value = "purchaseTime") Timestamp purchaseTime,
-                 @JsonProperty(required = true, value = "closeTime") Timestamp closeTime,
-                 @JsonProperty(required = true, value = "gainLostTotal") Double gainLostTotal,
-                 @JsonProperty(required = true, value = "status") StockStatus status,
-                 @JsonProperty(required = true, value = "tradePlatform") TradePlatform tradePlatform) {
-        this.stockKey = stockKey;
-        this.symbol = symbol;
-        this.fullName = fullName;
-        this.quantity = quantity;
-        this.openUnitCost = openUnitCost;
-        this.closeUnitCost = closeUnitCost;
-        this.purchaseTime = purchaseTime;
-        this.closeTime = closeTime;
-        this.gainLostTotal = gainLostTotal;
-        this.status = status;
-        this.tradePlatform = tradePlatform;
-    }
+public class Stock extends Model {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "stock_key", unique = true, nullable = false)
+    @Column(name = "stock_Key", unique = true, nullable = false)
     private Integer stockKey;
 
-    @Column(name = "symbol", unique = false, nullable = false)
+    @Column(name = "symbol", nullable = false)
     private String symbol;
 
-    @Column(name = "full_name", unique = false, nullable = false)
-    private String fullName;
+    @Column(name = "description", nullable = false)
+    private String description;
 
-    @Column(name = "quantity", unique = false, nullable = false)
-    private Integer quantity;
-
-    @Column(name = "open_unit_cost", unique = false, nullable = false)
-    private Double openUnitCost;
-
-    @Column(name = "close_unit_cost", unique = false, nullable = false)
-    private Double closeUnitCost;
-
-    @Column(name = "purchase_time", unique = false, nullable = false)
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
-    private Timestamp purchaseTime;
-
-    @Column(name = "close_time", unique = false, nullable = false)
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
-    private Timestamp closeTime;
-
-    @Column(name = "gain_lost_total", unique = false, nullable = false)
-    private Double gainLostTotal;
-
-    @Column(name = "status", nullable = false)
-    @Convert(converter = StockStatusConvert.class)
-    private StockStatus status;
-
-    @Column(name = "trade_platform", nullable = false)
-    @Convert(converter = TradePlatformConvert.class)
-    private TradePlatform tradePlatform;
+    @Column(name = "category", nullable = false)
+    private String category;
 
     public Integer getStockKey() {
         return stockKey;
@@ -99,92 +37,29 @@ public class Stock extends Ticket implements Serializable {
         this.symbol = symbol;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getDescription() {
+        return description;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public Integer getQuantity() {
-        return quantity;
+    public String getCategory() {
+        return category;
     }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getOpenUnitCost() {
-        return openUnitCost;
-    }
-
-    public void setOpenUnitCost(Double openUnitCost) {
-        this.openUnitCost = openUnitCost;
-    }
-
-    public Double getCloseUnitCost() {
-        return closeUnitCost;
-    }
-
-    public void setCloseUnitCost(Double closeUnitCost) {
-        this.closeUnitCost = closeUnitCost;
-    }
-
-    public Timestamp getPurchaseTime() {
-        return purchaseTime;
-    }
-
-    public void setPurchaseTime(Timestamp purchaseTime) {
-        this.purchaseTime = purchaseTime;
-    }
-
-    public Timestamp getCloseTime() {
-        return closeTime;
-    }
-
-    public void setCloseTime(Timestamp closeTime) {
-        this.closeTime = closeTime;
-    }
-
-    public Double getGainLostTotal() {
-        return gainLostTotal;
-    }
-
-    public void setGainLostTotal(Double gainLostTotal) {
-        this.gainLostTotal = gainLostTotal;
-    }
-
-    public StockStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(StockStatus status) {
-        this.status = status;
-    }
-
-    public TradePlatform getTradePlatform() {
-        return tradePlatform;
-    }
-
-    public void setTradePlatform(TradePlatform tradePlatform) {
-        this.tradePlatform = tradePlatform;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     @Override
     public String toString() {
-        return "Stock{" +
+        return "Symbol{" +
                 "stockKey=" + stockKey +
                 ", symbol='" + symbol + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", quantity=" + quantity +
-                ", openUnitCost=" + openUnitCost +
-                ", closeUnitCost=" + closeUnitCost +
-                ", purchaseTime=" + purchaseTime +
-                ", closeTime=" + closeTime +
-                ", gainLostTotal=" + gainLostTotal +
-                ", status=" + status +
-                ", tradePlatform=" + tradePlatform +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
                 '}';
     }
 }
