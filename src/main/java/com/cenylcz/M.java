a4d65239843b989e91bf298e1a5df4176766cb26
Max Cai <max.cai@verifi.com>
2021-04-25 22:24:30 -0700

    A

---
+++ src/main/java/com/cenylcz/M.java
@@ -0,0 +1,2 @@
+package com.cenylcz;public class M {
+}
--- src/main/java/com/cenylcz/Model.java
+++ src/main/java/com/cenylcz/Model.java
@@ -1,7 +1,4 @@
 package com.cenylcz;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
 public abstract class Model {
 }
